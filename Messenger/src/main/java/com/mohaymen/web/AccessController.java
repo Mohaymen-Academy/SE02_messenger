f0803d769d00682f033ed1b60817faa24d5937ba
Sara <smahmoodabadi2002@gmail.com>
2023-08-07 14:51:30 +0330

    add request part annotation

--- Messenger/src/main/java/com/mohaymen/web/AccessController.java
+++ Messenger/src/main/java/com/mohaymen/web/AccessController.java
@@ -1,24 +1,30 @@
 package com.mohaymen.web;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class AccessController {
+    
 
     @GetMapping("/access/signup-validation")
-    public String isValidSignUpInfo(String username, String email) {
+    public String isValidSignUpInfo(@RequestPart("username") String username,@RequestPart("email") String email) {
         return "OK";
     }
 
-    @GetMapping("access/signup")
-    public String signUp(String username, byte[] password, String email) {
+    @GetMapping("/access/signup")
+    public String signUp(@RequestPart("username") String username,
+                         @RequestPart("password") byte[] password,
+                         @RequestPart("email") String email) {
         return "OK";
     }
 
 
-    @GetMapping("access/login")
-    public String login(String username, byte[] password) {
+    @GetMapping("/access/login")
+    public String login(@RequestPart("username") String username,
+                         @RequestPart("password") byte[] password) {
         return "OK";
     }
 
