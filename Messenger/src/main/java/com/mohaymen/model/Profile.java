638bfd883740532b0b329ea3bc39fda67fb6b2f2
sanoooavi <81512968+sanoooavi@users.noreply.github.com>
2023-08-07 12:28:28 +0330

    Profile entity+ chatType enum added

---
+++ Messenger/src/main/java/com/mohaymen/model/Profile.java
@@ -0,0 +1,33 @@
+package com.mohaymen.model;
+
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.Getter;
+
+@Getter
+@Entity
+@Table(name = "Profile")
+public class Profile {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotEmpty
+    @Column(name = "handle", unique = true, nullable = false)
+    private String handle;
+
+    @NotEmpty
+    @Column(name = "profile_name", length = 255, nullable = false)
+    private String profileName;
+
+    @Column(name = "biography")
+    private String biography;
+
+    @Column(name = "member_count")
+    private Integer memberCount;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "chat_type", nullable = false)
+    private ChatType type;
+}
