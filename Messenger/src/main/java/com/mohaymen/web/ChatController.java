0c234ac092f30081907f23c7b2e8489d4a5143ad
codestars2 codestars2 <codestars2@abriment.local>
2023-08-20 11:41:02 +0330

    get message update

--- Messenger/src/main/java/com/mohaymen/web/ChatController.java
+++ Messenger/src/main/java/com/mohaymen/web/ChatController.java
@@ -200,6 +200,7 @@ public class ChatController {
             chatService.pinChat(userId, chatId);
             return ResponseEntity.ok().build();
         } catch (Exception e) {
+            logger.error("Failed to pin chat : " + e.getMessage());
             return ResponseEntity.badRequest().build();
         }
     }
