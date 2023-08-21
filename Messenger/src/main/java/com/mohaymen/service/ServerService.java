cf0c66a1765fefe1fafa0e5a3f2aa7143b7c579f
sanoooavi <81512968+sanoooavi@users.noreply.github.com>
2023-08-21 09:38:32 +0330

    bug fixed

--- Messenger/src/main/java/com/mohaymen/service/ServerService.java
+++ Messenger/src/main/java/com/mohaymen/service/ServerService.java
@@ -10,6 +10,7 @@ import lombok.Getter;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 @Getter
 @Service
@@ -25,22 +26,29 @@ public class ServerService {
                          MessageRepository messageRepository, ChatParticipantRepository cpRepository) {
         this.messageRepository = messageRepository;
         this.cpRepository = cpRepository;
-        if (profileRepository.findById(1L).isEmpty())
+        Optional<Profile> server_tmp = profileRepository.findById(1L);
+        Optional<Profile> baseAccount_tmp = profileRepository.findById(2L);
+        Optional<Profile> baseChannel_tmp = profileRepository.findById(3L);
+        if (server_tmp.isEmpty())
             server = createServerAccounts(profileRepository, false, 3, 1L, "SERVER", "#SERVER");
-        if (profileRepository.findById(2L).isEmpty()) {
+        else
+            server = server_tmp.get();
+        if (baseAccount_tmp.isEmpty()) {
             baseAccount = createServerAccounts(profileRepository, false, 0, 2L, "اعلان های رسا", "#MESSENGER-BASE-ACCOUNT");
             baseAccount.setDefaultProfileColor("#808000");
             profileRepository.save(baseAccount);
-        }
-        if (profileRepository.findById(3L).isEmpty()) {
+        } else
+            baseAccount = baseAccount_tmp.get();
+        if (baseChannel_tmp.isEmpty()) {
             baseChannel = createServerAccounts(profileRepository, false, 2, 3L, "پیام رسان رسا", "#MESSENGER-BASE-CHANNEL");
-            sendMessage("این کانال ساخته شد",baseChannel);
+            sendMessage("این کانال ساخته شد", baseChannel);
             cpRepository.save(new ChatParticipant(baseAccount, baseChannel, baseChannel.getHandle(), true));
             baseChannel.setDefaultProfileColor("#008000");
             baseChannel.setMemberCount(1);
-
             profileRepository.save(baseChannel);
-        }
+        } else
+            baseChannel = baseChannel_tmp.get();
+
     }
 
     private Profile createServerAccounts(ProfileRepository profileRepository, boolean isDeleted, int type,
