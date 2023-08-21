2ca0a26756b602fac8251e0add96341c3b2d1894
Sara <smahmoodabadi2002@gmail.com>
2023-08-21 13:09:37 +0330

    set json view for media display

--- Messenger/src/main/java/com/mohaymen/model/json_item/MediaDisplay.java
+++ Messenger/src/main/java/com/mohaymen/model/json_item/MediaDisplay.java
@@ -1,5 +1,6 @@
 package com.mohaymen.model.json_item;
 
+import com.fasterxml.jackson.annotation.JsonView;
 import com.mohaymen.model.entity.Message;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -9,9 +10,16 @@ import java.util.List;
 @AllArgsConstructor
 public class MediaDisplay {
 
+    @JsonView(Views.GetMedia.class)
     private List<Message> images;
+
+    @JsonView(Views.GetMedia.class)
     private List<Message> voices;
+
+    @JsonView(Views.GetMedia.class)
     private List<Message> musics;
+
+    @JsonView(Views.GetMedia.class)
     private List<Message> files;
 
 }
