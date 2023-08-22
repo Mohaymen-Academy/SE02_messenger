e5a9de6578114dea741b95a8b48df62581d2ff91
Sara <smahmoodabadi2002@gmail.com>
2023-08-22 15:50:41 +0330

    clean log service

--- Messenger/src/main/java/com/mohaymen/service/LogService.java
+++ Messenger/src/main/java/com/mohaymen/service/LogService.java
@@ -7,6 +7,7 @@ import java.time.LocalDateTime;
 public class LogService {
 
     private enum Level{INFO, DEBUG, ERROR}
+
     private final LogRepository logRepository;
 
     private final String logger;
