421ca0b449b2f670f956d798b8b312adba910773
Sara <smahmoodabadi2002@gmail.com>
2023-08-22 11:04:06 +0330

    time formatter added

--- Messenger/src/main/java/com/mohaymen/model/json_item/MessageDisplay.java
+++ Messenger/src/main/java/com/mohaymen/model/json_item/MessageDisplay.java
@@ -4,8 +4,9 @@ import com.fasterxml.jackson.annotation.JsonView;
 import com.mohaymen.model.entity.Message;
 import com.mohaymen.model.entity.Profile;
 import lombok.Getter;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 
 @JsonView(Views.GetMessage.class)
@@ -19,6 +20,7 @@ public class MessageDisplay {
     private final List<Message> messages;
 
     private final Long messageId;
+    private final DateTimeFormatter formatter;
 
     public MessageDisplay(List<Message> upMessages,
                           List<Message> downMessages,
@@ -26,6 +28,8 @@ public class MessageDisplay {
                           boolean isDownFinished,
                           boolean isUpFinished,
                           Profile serverProfile) {
+        formatter = DateTimeFormatter
+                .ofPattern("yyyy-MM-dd").withZone(ZoneId.of("Asia/Tehran"));
         this.isDownFinished = isDownFinished;
         this.isUpFinished = isUpFinished;
         this.messages = new ArrayList<>();
@@ -47,7 +51,7 @@ public class MessageDisplay {
             return;
         }
         Message lastMessage = messages.get(messages.size() - 1);
-        if (!Date.from(lastMessage.getTime()).toString().equals(Date.from(message.getTime()).toString())) {
+        if (!formatter.format(lastMessage.getTime()).equals(formatter.format(message.getTime()))) {
             messages.add(getServerMessage(message, serverProfile));
         }
         messages.add(message);
@@ -56,7 +60,7 @@ public class MessageDisplay {
     private Message getServerMessage(Message message, Profile serverProfile) {
         Message serverMessage = new Message();
         serverMessage.setMessageID(0L);
-        serverMessage.setText(Date.from(message.getTime()).toString());
+        serverMessage.setText(formatter.format(message.getTime()));
         serverMessage.setTime(message.getTime());
         serverMessage.setSender(serverProfile);
         serverMessage.setReceiver(message.getReceiver());
