8756e399b97deb4446e45829b7cdd2ef253558bf
Sara <smahmoodabadi2002@gmail.com>
2023-08-22 15:52:50 +0330

    clean pin chat service

--- Messenger/src/main/java/com/mohaymen/service/PinChatService.java
+++ Messenger/src/main/java/com/mohaymen/service/PinChatService.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Service;
 @Service
 public class PinChatService extends PinService {
 
-
     public PinChatService(ChatParticipantRepository cpRepository,
                           BlockRepository blockRepository,
                           ProfileRepository profileRepository,
